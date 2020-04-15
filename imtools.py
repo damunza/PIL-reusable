661fe35662dea0b9bd44c070524c1cbb215f6c5d
damunza <danidamunza@gmail.com>
2020-04-15 17:41:01 +0300

    the function works and brings the exact number of images in a directory

--- imtools.py
+++ imtools.py
@@ -8,7 +8,7 @@ def get_images(path, types):
     # images for the first type
     first = [os.path.join(path, i) for i in os.listdir(path) if i.endswith(types[a])]
     # images for every type offered 
-    if a <= len(types):
+    while a < (len(types) - 1):
         a +=1
         first += [os.path.join(path, i) for i in os.listdir(path) if i.endswith(types[a])]
 
