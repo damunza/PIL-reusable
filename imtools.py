47517b7316a3f009da86bf21721d7272dcf7a4a4
damunza <danidamunza@gmail.com>
2020-04-15 17:25:48 +0300

    created a file to hold all repeated functions and a function to get all image types from a directory

---
+++ imtools.py
@@ -0,0 +1,15 @@
+import os
+
+def get_images(path, types):
+    """
+    takes in the directory and list of image formats and returns a list of all the images
+    """
+    a = 0
+    # images for the first type
+    first = [os.path.join(path, i) for i in os.listdir(path) if i.endswith(types[a])]
+    # images for every type offered 
+    if a <= len(types):
+        a +=1
+        first += [os.path.join(path, i) for i in os.listdir(path) if i.endswith(types[a])]
+
+    return first
--- open.py
+++ open.py
@@ -1,6 +1,11 @@
 from PIL import Image
+import os
 
 pil_im = Image.open('/home/name/Downloads/IMG-20200413-WA0005.jpg')
 
-pil_im.show()
+# pil_im.show()
 # if using a home directory file use the sudo command
+
+x = os.listdir('/home/name/Downloads/')
+
+print(x)
