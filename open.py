bf8653a596a8af43d1076d2155ae2bf18d26427c
damunza <danidamunza@gmail.com>
2020-04-16 12:58:20 +0300

    resized and rotated an image then saved it

--- open.py
+++ open.py
@@ -4,12 +4,20 @@ import os
 
 x = GI('/home/name/Downloads/',['.jpg', '.png'])
 
-pil_im = Image.open(x[1])
-
+# croping and save image 
+im_two = Image.open(x[1])
 box = (100,100,400,400)
-work = pil_im.copy()
+work = im_two.copy()
 region = work.crop(box)
 region.save('region.jpg')
 
+# resize and rotate 
+im_three = Image.open(x[2])
+new_size = (512, 512)
+copy_3 = im_three.copy()
+work_3 = copy_3.resize(new_size)
+rot_3 = work_3.rotate(45)
+rot_3.save('im3.jpg')
+
 # displaying the image 
-region.show()
+rot_3.show()
